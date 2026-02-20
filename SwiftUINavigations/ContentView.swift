e99884c9b7e31e4f65550b4c8d5971c577fee03d
Vidhyapathi Kandhasamy <vidhyapathicse@gmail.com>
2026-02-20 16:57:06 +0530

    Navigation Link
    1. The Navigation Title and label functionality has been implemented and explained.
    
    2. The Navigation Destination label functionality has also been explained.

--- SwiftUINavigations/ContentView.swift
+++ SwiftUINavigations/ContentView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var body: some View {
+        Text("Detail View")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            Text("Hello World!")
+            Spacer()
+            NavigationLink("Navigation Title") {
+                DetailView()
+            }
+            Spacer()
+            NavigationLink {
+                DetailView()
+            } label: {
+                Text("Go to Detail")
+            }
+            Spacer()
         }
     }
 }
