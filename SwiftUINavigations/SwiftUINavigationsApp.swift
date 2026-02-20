95bfc65b7d37175f317d448d57827104122b81b8
Vidhyapathi Kandhasamy <vidhyapathicse@gmail.com>
2026-02-20 16:35:55 +0530

    Initial Commit

---
+++ SwiftUINavigations/SwiftUINavigationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationsApp.swift
+//  SwiftUINavigations
+//
+//  Created by Vidhyapathi on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
